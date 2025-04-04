09f22cc0209233ab519f00f9f03bead4a7cb102f
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-04-04 16:43:45 +0200

    implement API calls to fetch dashboard data for city, weather, and airport information

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('ciao');
+async function getDashboardData(query) {
+    console.log(`Carico la query di ${query}`);
+
+    const destinationPromise = fetch(`https://freetestapi.com/api/v1/destinations?search=${query}`).then( res => res.json())
+    const weathersPromise = fetch(`https://freetestapi.com/api/v1/weathers?search=${query}`).then( res => res.json())
+    const airportsPromise = fetch(`https://freetestapi.com/api/v1/airports?search=${query}`).then( res => res.json())
+    
+    const promises = [destinationPromise , weathersPromise , airportsPromise]
+
+    const promisesResults = await Promise.all(promises);
+
+    const destinations = promisesResults[0];
+    const weathers = promisesResults[1];
+    const airports = promisesResults[2];
+
+    return {
+        city: destinations[0].name,
+        country: destinations[0].country,
+        temperature: weathers[0].temperature,
+        weather: weathers[0].weather_description,
+        airport: airports[0].name
+    }
+}
+
+getDashboardData('london')
+    .then(data => {
+        console.log('Dasboard data:', data);
+        console.log(
+            `${data.city} is in ${data.country}.\n` +
+            `Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n`+
+            `The main airport is ${data.airport}.\n`
+        );
+    })
+    .catch(error => console.error(error));
